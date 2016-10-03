b57f983d3aef3fc5f6682fe1550bb6518832ce96
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-03 09:53:01 +0200

    [REM] stock_quant_merge: nhrgfnhtngftf tg y g yhyh y h yh y hyhyhy yhyhyhyhyhyhyhy  yhyh

--- stock_quant_merge/__openerp__.py
+++ stock_quant_merge/__openerp__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock - Quant merge",
-    "version": "9.0.1.0.0",
+    "version": "9.0.1.0.1",
     "depends": [
         "stock",
     ],
